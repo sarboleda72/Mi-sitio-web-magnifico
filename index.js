a469a4d55548d3d2c6245c82e500f5db85680d67
sarboleda72 <sarboleda72@gmail.com>
2022-12-05 19:48:40 -0500

    Se hacen ajustes en el html para que el menú cierre y abrá

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu= document.querySelector(".nav-menu");
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
 
-navToggle.addEventListener("click", ()=>{
+navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 
-if(navMenu.classList.contains("nav-menu_visible")){
-    navToggle.setAttribute("aria-label","cerrar menú");
+
+if (navMenu.classList.contains("nav-menu_visible")){
+    navToggle.setAttribute("aria-label", "Cerrar menu")
 }else{
-    navToggle.setAttribute("aria-label","abrir menú")
+    navToggle.setAttribute("aria-label", "Abrir menu")
 }
-});
+})
